db82c9b7bfde29ba8fdf9f0eb7d910b9126f8d2d
Manar <manarbouoni@gmail.com>
2023-12-13 22:59:02 +0100

    version 1

---
+++ spring-security-webapp-master/src/main/java/de/bsi/security/SecurityConfiguration.java
@@ -0,0 +1,50 @@
+package de.bsi.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+	
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests()
+			.requestMatchers("/admin").hasRole("ADMIN")
+			.anyRequest().permitAll()
+		.and().formLogin()
+		.and().logout().logoutSuccessUrl("/index");
+		return http.build();
+	}
+	
+	@Bean
+	UserDetailsService users(@Autowired PasswordEncoder pwEnc) {
+	    UserDetails user = User.builder()
+	        .username("user")
+	        .password(pwEnc.encode("user"))
+	        .roles("USER")
+	        .build();
+	    UserDetails admin = User.builder()
+	        .username("admin")
+	        .password(pwEnc.encode("admin"))
+	        .roles("USER", "ADMIN")
+	        .build();
+	    return new InMemoryUserDetailsManager(user, admin);
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+}
